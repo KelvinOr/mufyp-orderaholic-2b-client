bb1bfa623dcaa0fffbe803a1fc97fdca21c90f85
Akatsuki <55689974+KelvinOr@users.noreply.github.com>
2022-09-13 16:32:56 +0800

    commit

--- src/Pages/SelectLoginPage/SelectLoginPage.jsx
+++ src/Pages/SelectLoginPage/SelectLoginPage.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import '../GlobalStyle.css';
 import "./SelectLoginPage.css";
-import { Form, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import '../Theme.scss';
 
 class SelectLoginPage extends React.Component {
 
@@ -22,25 +19,7 @@ class SelectLoginPage extends React.Component {
                 </div>
                 <div className="main">
                     <div className="JoinUs">
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Corfirm Password</Form.Label>
-                                <Form.Control type="password" placeholder="Corfirm Password" />
-                            </Form.Group>
-                            <Button variant="secondary">
-                                Sign Up
-                            </Button>
-                        </Form>
+                        
                     </div>
                 </div>
             </div>
