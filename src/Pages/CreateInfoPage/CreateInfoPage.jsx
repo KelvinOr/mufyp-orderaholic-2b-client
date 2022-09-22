99fbe4d6c4dddefb9f32d0a00839da37dea1331e
Akatsuki <orkevin947@gmail.com>
2022-09-22 21:37:48 +0800

    commit

--- src/Pages/CreateInfoPage/CreateInfoPage.jsx
+++ src/Pages/CreateInfoPage/CreateInfoPage.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import "../GlobalStyle.css";
 import styles from "./CreateInfoPage.module.css";
 import { CustomTheme } from '../../Config/Color';
+import { Button, Grid } from "@mui/material";
 
 export default class CreateInfoPage extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+        };
+
+        this.CreateInfoForm = {
+          defaultImage: "https://protkd.com/wp-content/uploads/2017/04/default-image.jpg",
         };
     }
 
@@ -20,6 +25,15 @@ export default class CreateInfoPage extends React.Component {
         </div>
         <div className={styles.backgroundCard} style={{background: CustomTheme.secondary}} />
         <div className={styles.main}>
+          <Grid container spacing={3} >
+            <Grid item xs={4}>
+              <img src={this.CreateInfoForm.defaultImage} className={styles.image}/><br/>
+              <div className={styles.text} >This image is using to show restaurant.</div>
+            </Grid>
+            <Grid item xs={2}>
+              <Button>test</Button>
+            </Grid>
+          </Grid>
           
         </div>
       </div>
