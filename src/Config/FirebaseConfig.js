5464e03b0f6f11b8fb91bc5ff3045e88557a2cfe
Akatsuki <orkevin947@gmail.com>
2022-09-28 21:20:17 +0800

    commit

--- src/Config/FirebaseConfig.js
+++ src/Config/FirebaseConfig.js
@@ -7,7 +7,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_apiKey}`,
   authDomain: `${process.env.REACT_APP_authDomain}`,
   projectId: `${process.env.REACT_APP_projectId}`,
