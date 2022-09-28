7e083b5127b8accced288e1e4ccc21b88ff8105c
Akatsuki <55689974+KelvinOr@users.noreply.github.com>
2022-09-28 11:27:57 +0800

    commit

--- src/Pages/CreateInfoPage/CreateInfoPage.jsx
+++ src/Pages/CreateInfoPage/CreateInfoPage.jsx
@@ -18,6 +18,9 @@ export default class CreateInfoPage extends React.Component {
         this.CreateInfoForm = {
           Image: "https://protkd.com/wp-content/uploads/2017/04/default-image.jpg",
           Name: "",
+          Type: "",
+          ContectNumber: "",
+          Location: "",
         };
 
         this.buttonPrimaryColor = {
@@ -51,7 +54,7 @@ export default class CreateInfoPage extends React.Component {
         </div>
         <div className={styles.backgroundCard} style={{background: CustomTheme.secondary}} />
         <div className={styles.main}>
-          <div className={styles.gridRow}>
+          <div className={styles.gridRow1}>
 
             <div>
               {this.state.imageIsUpdate?  <img src={this.CreateInfoForm.Image} className={styles.image} alt="Restaurant"/>: <img src={this.CreateInfoForm.Image} className={styles.image} alt="Restaurant"/>}
@@ -90,8 +93,36 @@ export default class CreateInfoPage extends React.Component {
               </div>
             </div>
 
-            <div>
-              
+          </div>
+
+          <div className={styles.gridRow2}>
+
+            <div style={{height: "100%", width:"40%", display: "flex", alignItems: "center", marginLeft: "20px"}}>
+              <div style={{display: "flex", flexDirection:"column", width: "100%"}}>
+                <div className={styles.text}>
+                  Contect Number:
+                </div>
+                <Paper style={this.InputPrimaryColor}>
+                  <InputBase size='large' placeholder="Input Contect Number" sx={{p: '5px'}} style={{ color: "#ffffff"}} onChange={(event) => {this.CreateInfoForm.ContectNumber = event.target.value}}/>
+                </Paper>
+                <div className={styles.text}>
+                  Restaurant Location
+                </div>
+                <Paper style={this.InputPrimaryColor}>
+                  <InputBase size='large' placeholder="Input Restaurant Location" sx={{p: '5px'}} style={{ color: "#ffffff"}} onChange={(event) => {this.CreateInfoForm.Location = event.target.value}}/>
+                </Paper>
+              </div>
+            </div>
+
+            <div style={{height: "100%", width:"40%", display: "flex", alignItems: "center", marginRight: "20px"}}>
+              <div style={{display: "flex", flexDirection:"column", width: "100%"}}>
+                <div className={styles.text}>
+                  Discription:
+                </div>
+                <Paper style={{...this.InputPrimaryColor, height: "10%"}}>
+                  <InputBase size='large' placeholder="Input Restaurant Discription" sx={{p: '5px'}} style={{ color: "#ffffff"}} onChange={(event) => {this.CreateInfoForm.Discription = event.target.value}}/>
+                </Paper>
+              </div>
             </div>
 
           </div>
