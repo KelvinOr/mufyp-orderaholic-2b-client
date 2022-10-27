36fe2208d2ffc7e8bd49ec277fe34c73eb3d5c11
Akatsuki <55689974+KelvinOr@users.noreply.github.com>
2022-10-27 14:20:48 +0800

    commit

--- src/Pages/OrderListPage/OrderListPage.jsx
+++ src/Pages/OrderListPage/OrderListPage.jsx
@@ -1,20 +1,48 @@
 import React from "react";
 import styles from "./OrderListPage.module.css";
+import { 
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from "@mui/material";
 
 export default class OrderListPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        isLoading: true,
-        OrderList: [],
+            isLoading: true,
+            OrderList: [],
+            DialogOpen: false,
+
         };
     }
     
     render() {
         return (
-        <div className={styles.mainContainer}>
+            <div className={styles.mainContainer}>
+                <Dialog open={this.state.DialogOpen} PaperProps={{ style: { backgroundColor: CustomTheme.primary, width: "50%" }}}>
 
-        </div>
+                    <DialogTitle>
+                        <div className={styles.text}>New Order:</div>
+                    </DialogTitle>
+
+                    <DialogContent>
+
+                        <DialogContentText>
+                            
+                        </DialogContentText> 
+
+                    </DialogContent>
+
+                    <DialogActions>
+                        
+                    </DialogActions>
+
+                </Dialog>
+            </div>
         );
     }
     }
