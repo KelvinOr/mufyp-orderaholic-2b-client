a9a3dd050295e5713480dadd7eb8b2a8677c9527
akatsuki <orkevin947@gmail.com>
2023-05-04 10:54:11 +0800

    commit

--- src/Config/RedirectPath.js
+++ src/Config/RedirectPath.js
@@ -2,13 +2,13 @@ export default function RedirectPaht(){
     if (window.location.hostname === "localhost" || window.location.hostname === "" | window.location.hostname === "127.0.0.1") {
         return {
             home: "/",
-            createInfo: "/createInfo",
+            createInfo: "/create-info",
             disboard: "/disboard",
         }
     } else {
         return {
             home: "/",
-            createInfo: "/createInfo",
+            createInfo: "/create-info",
             disboard: "/disboard",
         }
     }
